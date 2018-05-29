5f7e1bf73ab3d7d58b28440b7d3267bde5042af8
win10.yibaijin.lirenbo <447430787@qq.com>
2018-05-29 17:38:51 +0800

    '2018年5月29日17:38:49'

--- service-boot-web/src/main/java/com/yibaijin/service/boot/web/config/SecurityConfiguration.java
+++ service-boot-web/src/main/java/com/yibaijin/service/boot/web/config/SecurityConfiguration.java
@@ -1,34 +1,43 @@
-package com.yibaijin.service.boot.web.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
-
-@EnableWebSecurity
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/css/**", "/index").permitAll()
-                .antMatchers("/c/**").hasRole("USER")
-                .and()
-                .formLogin()
-                .loginPage("/login").failureUrl("/login-error")
-                .and().httpBasic();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser("user").password("password").roles("USER");
-    }
-
-}
+//package com.yibaijin.service.boot.web.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//
+//import javax.sql.DataSource;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().antMatchers("/oauth/**").authenticated().and().csrf()
+//                .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize")).disable().formLogin()
+//                .permitAll().and().logout().permitAll().and();
+//    }
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+//                .withUser("user").password("password").roles("USER");
+////        User.UserBuilder users = User.withDefaultPasswordEncoder();
+////        AuthenticationProvider authenticationProvider=new A;
+////        auth
+////                .authenticationProvider(authenticationProvider).jdbcAuthentication()
+////                .dataSource(dataSource)
+////                .withDefaultSchema()
+////                .withUser(users.username("user").password("password").roles("USER"))
+////                .withUser(users.username("admin").password("password").roles("USER","ADMIN"));
+//
+//    }
+//
+//}
